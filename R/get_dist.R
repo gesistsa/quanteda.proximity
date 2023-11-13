53bd1e245ae76755b0b8c6c3367153253790da42
chainsawriot <chainsawtiney@gmail.com>
2023-11-13 17:11:03 +0100

    Add `valuetype`

--- R/get_dist.R
+++ R/get_dist.R
@@ -24,12 +24,26 @@ get_dist <- function(x, targets, get_min = TRUE) {
     purrr::map(x, .get_dist, targets_poss = targets_poss, get_min = get_min)
 }
 
+.resolve_targets <- function(targets, features, valuetype) {
+    if (valuetype == "fixed") {
+        return(targets)
+    }
+    if (valuetype == "glob") {
+        regex <- paste(glob2rx(targets), collapse = "|")
+    }
+    if (valuetype == "regex") {
+        regex <- paste(targets, collapse = "|")
+    }
+    grep(regex, features, value = TRUE)
+}
+
 #' Extract Distance Information
 #'
 #' This function extracts distance information from a [quanteda::tokens()] object.
 #' @param x a `tokens` object
 #' @param targets a character vector of anchor words
 #' @param get_min logical, whether to return only the minimum distance or raw distance information; it is more relevant when `targets` have more than one word. See details.
+#' @param valuetype See [quanteda::valuetype]
 #' @details The distance is measured by number of tokens away from the target. Given a tokenized sentence: ["I", "eat", "this", "apple"] and suppose "eat" is the target. The vector of minimum distances for each word from "eat" is [2, 1, 2, 3]. In another case: ["I", "wash", "and", "eat", "this", "apple"] and ["wash", "eat"] are the targets. The minimal distance vector is [2, 1, 2, 1, 2, 3]. If `get_min` is `FALSE`, the output is a list of two vectors. For "wash", the distance vector is [1, 0, 1, 2, 3]. For "eat", [3, 2, 1, 0, 1, 2]. `get_min` always add 1 to the distance.
 #' It is recommended to conduct all text maniputation tasks with all `tokens_*()` functions before calling this function.
 #' @return a `tokens_with_dist` object. It is a derivative of [quanteda::tokens()], i.e. all `token_*` functions still work. A `tokens_with_dist` has a modified [print()] method. Also, additional data slots are included
@@ -39,7 +53,7 @@ get_dist <- function(x, targets, get_min = TRUE) {
 #' @examples
 #' library(quanteda)
 #' ukimg_eu <- data_char_ukimmig2010 %>% tokens(remove_punct = TRUE) %>%
-#' tokens_tolower() %>% tokens_dist(c("eu", "europe", "european"))
+#' tokens_tolower() %>% tokens_dist(c("eu", "euro*"))
 #' ukimg_eu %>% dfm() %>% dfm_select(c("immig*", "migr*")) %>% rowSums() %>% sort()
 #' ## compare with
 #' data_char_ukimmig2010 %>% tokens(remove_punct = TRUE) %>% tokens_tolower() %>%
@@ -48,10 +62,12 @@ get_dist <- function(x, targets, get_min = TRUE) {
 #' ukimg_eu %>% tokens_dist("britain")
 #' @seealso [dfm.tokens_with_dist()] [quanteda::tokens()]
 #' @export
-tokens_dist <- function(x, targets, get_min = TRUE) {
+tokens_dist <- function(x, targets, get_min = TRUE, valuetype = c("glob", "regex", "fixed")) {
     if (!inherits(x, "tokens")) {
         stop("x is not a `tokens` object.", call. = FALSE)
     }
+    valuetype <- match.arg(valuetype)
+    targets <- .resolve_targets(targets, attr(x, "types"), valuetype)
     toks <- x
     dist <- get_dist(x = toks, targets = targets, get_min = get_min)
     quanteda::docvars(toks)$dist <- I(dist)
--- tests/testthat/test-tokens_dist.R
+++ tests/testthat/test-tokens_dist.R
@@ -1,3 +1,9 @@
 test_that("defensive", {
     expect_error(tokens_dist("a", "a"))
 })
+
+test_that(".resolve_targets", {
+    expect_equal(.resolve_targets(c("abc", "def"), c("abcd", "defa"), valuetype = "fixed"), c("abc", "def"))
+    expect_equal(.resolve_targets(c("abc*", "def*"), c("abcd", "defa"), valuetype = "glob"), c("abcd", "defa"))
+    expect_equal(.resolve_targets(c("a"), c("abcd", "defa"), valuetype = "regex"), c("abcd", "defa"))
+})
