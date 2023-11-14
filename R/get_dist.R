9132c28ae2de02caba9432d72b9276223b13445b
chainsawriot <chainsawtiney@gmail.com>
2023-11-14 10:56:54 +0100

    Package rename fix #13

--- R/get_dist.R
+++ R/get_dist.R
@@ -6,8 +6,8 @@
     min(purrr::map_dbl(x, pos))
 }
 
-.get_dist <- function(tokenized_text, targets_poss, get_min = TRUE) {
-    target_idx <- which(tokenized_text %in% targets_poss)
+.get_proximity <- function(tokenized_text, keywords_poss, get_min = TRUE) {
+    target_idx <- which(tokenized_text %in% keywords_poss)
     poss <- seq_along(tokenized_text)
     if (length(target_idx) == 0) {
         return(rep(length(poss) + 1, length(poss)))
@@ -19,20 +19,20 @@
     return(res)
 }
 
-get_dist <- function(x, targets, get_min = TRUE) {
-    targets_poss <- which(attr(x, "types") %in% targets)
-    purrr::map(x, .get_dist, targets_poss = targets_poss, get_min = get_min)
+get_proximity <- function(x, keywords, get_min = TRUE) {
+    keywords_poss <- which(attr(x, "types") %in% keywords)
+    purrr::map(x, .get_proximity, keywords_poss = keywords_poss, get_min = get_min)
 }
 
-.resolve_targets <- function(targets, features, valuetype) {
+.resolve_keywords <- function(keywords, features, valuetype) {
     if (valuetype == "fixed") {
-        return(targets)
+        return(keywords)
     }
     if (valuetype == "glob") {
-        regex <- paste(glob2rx(targets), collapse = "|")
+        regex <- paste(glob2rx(keywords), collapse = "|")
     }
     if (valuetype == "regex") {
-        regex <- paste(targets, collapse = "|")
+        regex <- paste(keywords, collapse = "|")
     }
     grep(regex, features, value = TRUE)
 }
@@ -41,73 +41,73 @@ get_dist <- function(x, targets, get_min = TRUE) {
 #'
 #' This function extracts distance information from a [quanteda::tokens()] object.
 #' @param x a `tokens` object
-#' @param targets a character vector of anchor words
-#' @param get_min logical, whether to return only the minimum distance or raw distance information; it is more relevant when `targets` have more than one word. See details.
+#' @param keywords a character vector of anchor words
+#' @param get_min logical, whether to return only the minimum distance or raw distance information; it is more relevant when `keywords` have more than one word. See details.
 #' @param valuetype See [quanteda::valuetype]
-#' @details The distance is measured by number of tokens away from the target. Given a tokenized sentence: ["I", "eat", "this", "apple"] and suppose "eat" is the target. The vector of minimum distances for each word from "eat" is [2, 1, 2, 3]. In another case: ["I", "wash", "and", "eat", "this", "apple"] and ["wash", "eat"] are the targets. The minimal distance vector is [2, 1, 2, 1, 2, 3]. If `get_min` is `FALSE`, the output is a list of two vectors. For "wash", the distance vector is [1, 0, 1, 2, 3]. For "eat", [3, 2, 1, 0, 1, 2]. `get_min` always add 1 to the distance.
+#' @details The distance is measured by number of tokens away from the target. Given a tokenized sentence: ["I", "eat", "this", "apple"] and suppose "eat" is the target. The vector of minimum distances for each word from "eat" is [2, 1, 2, 3]. In another case: ["I", "wash", "and", "eat", "this", "apple"] and ["wash", "eat"] are the keywords. The minimal distance vector is [2, 1, 2, 1, 2, 3]. If `get_min` is `FALSE`, the output is a list of two vectors. For "wash", the distance vector is [1, 0, 1, 2, 3]. For "eat", [3, 2, 1, 0, 1, 2]. `get_min` always add 1 to the distance.
 #' It is recommended to conduct all text maniputation tasks with all `tokens_*()` functions before calling this function.
-#' @return a `tokens_with_dist` object. It is a derivative of [quanteda::tokens()], i.e. all `token_*` functions still work. A `tokens_with_dist` has a modified [print()] method. Also, additional data slots are included
+#' @return a `tokens_with_proximity` object. It is a derivative of [quanteda::tokens()], i.e. all `token_*` functions still work. A `tokens_with_proximity` has a modified [print()] method. Also, additional data slots are included
 #' * a document variation `dist`
-#' * a metadata slot `targets`
+#' * a metadata slot `keywords`
 #' * a metadata slot `get_min`
 #' @examples
 #' library(quanteda)
 #' ukimg_eu <- data_char_ukimmig2010 %>% tokens(remove_punct = TRUE) %>%
-#' tokens_tolower() %>% tokens_dist(c("eu", "euro*"))
+#' tokens_tolower() %>% tokens_proximity(c("eu", "euro*"))
 #' ukimg_eu %>% dfm() %>% dfm_select(c("immig*", "migr*")) %>% rowSums() %>% sort()
 #' ## compare with
 #' data_char_ukimmig2010 %>% tokens(remove_punct = TRUE) %>% tokens_tolower() %>%
 #' dfm %>% dfm_select(c("immig*", "migr*")) %>% rowSums() %>% sort()
-#' ## rerun to select other targets
-#' ukimg_eu %>% tokens_dist("britain")
-#' @seealso [dfm.tokens_with_dist()] [quanteda::tokens()]
+#' ## rerun to select other keywords
+#' ukimg_eu %>% tokens_proximity("britain")
+#' @seealso [dfm.tokens_with_proximity()] [quanteda::tokens()]
 #' @export
-tokens_dist <- function(x, targets, get_min = TRUE, valuetype = c("glob", "regex", "fixed")) {
+tokens_proximity <- function(x, keywords, get_min = TRUE, valuetype = c("glob", "regex", "fixed")) {
     if (!inherits(x, "tokens")) {
         stop("x is not a `tokens` object.", call. = FALSE)
     }
     valuetype <- match.arg(valuetype)
-    targets <- .resolve_targets(targets, attr(x, "types"), valuetype)
+    keywords <- .resolve_keywords(keywords, attr(x, "types"), valuetype)
     toks <- x
-    dist <- get_dist(x = toks, targets = targets, get_min = get_min)
-    quanteda::docvars(toks)$dist <- I(dist)
-    quanteda::meta(toks, field = "targets") <- targets
+    proximity <- get_proximity(x = toks, keywords = keywords, get_min = get_min)
+    quanteda::docvars(toks)$proximity <- I(proximity)
+    quanteda::meta(toks, field = "keywords") <- keywords
     quanteda::meta(toks, field = "get_min") <- get_min
-    class(toks) <- c("tokens_with_dist", "tokens")
+    class(toks) <- c("tokens_with_proximity", "tokens")
     return(toks)
 }
 
-#' @method print tokens_with_dist
+#' @method print tokens_with_proximity
 #' @export
-print.tokens_with_dist <- function(x, ...) {
+print.tokens_with_proximity <- function(x, ...) {
     y <- x
     class(y) <- "tokens"
     print(y, ...)
     cat("With distance vector(s).\n")
-    cat("targets: ", quanteda::meta(x, field = "targets"), "\n")
+    cat("keywords: ", quanteda::meta(x, field = "keywords"), "\n")
 }
 
 
 #' Create a document-feature matrix
 #'
-#' Construct a sparse document-feature matrix from the output of [tokens_dist()].
-#' @param x output of [tokens_dist()]
-#' @param remove_docvars_dist boolean, remove the "dist" document variable
+#' Construct a sparse document-feature matrix from the output of [tokens_proximity()].
+#' @param x output of [tokens_proximity()]
+#' @param remove_docvars_proximity boolean, remove the "proximity" document variable
 #' @param weight_function a weight function, default to invert distance
 #' @importFrom quanteda dfm
-#' @details By default, words closer to targets are weighted higher. You might change that with another `weight_function`.
+#' @details By default, words closer to keywords are weighted higher. You might change that with another `weight_function`.
 #' @examples
 #' library(quanteda)
 #' ukimg_eu <- data_char_ukimmig2010 %>% tokens(remove_punct = TRUE) %>%
-#' tokens_tolower() %>% tokens_dist(c("eu", "europe", "european"))
+#' tokens_tolower() %>% tokens_proximity(c("eu", "europe", "european"))
 #' ukimg_eu %>% dfm() %>% dfm_select(c("immig*", "migr*")) %>% rowSums() %>% sort()
-#' ## Words further away from targets are weighted higher
+#' ## Words further away from keywords are weighted higher
 #' ukimg_eu %>% dfm(weight_function = identity) %>% dfm_select(c("immig*", "migr*")) %>% rowSums() %>% sort()
 #' ukimg_eu %>% dfm(weight_function = function(x) {1 / x^2}) %>%
 #' dfm_select(c("immig*", "migr*")) %>% rowSums() %>% sort()
-#' @method dfm tokens_with_dist
+#' @method dfm tokens_with_proximity
 #' @export
-dfm.tokens_with_dist <- function(x, remove_docvars_dist = TRUE,
+dfm.tokens_with_proximity <- function(x, remove_docvars_proximity = TRUE,
                                  weight_function = function(x) {1 / x}, ...) {
     vec <- c() ## value (x) in the sparseMatrix
     i_pos <- c()
@@ -116,7 +116,7 @@ dfm.tokens_with_dist <- function(x, remove_docvars_dist = TRUE,
     x_attrs <- attributes(x)
     x_docvars <- docvars(x)
     for (i in seq_along(x)) {
-        cur_dist <- quanteda::docvars(x, "dist")[[i]]
+        cur_dist <- quanteda::docvars(x, "proximity")[[i]]
         cur_feat <- match(x[[i]], feat_name)
         unique_feat <- unique(cur_feat)
         total_vec <- rep(0, length(unique_feat))
@@ -134,8 +134,8 @@ dfm.tokens_with_dist <- function(x, remove_docvars_dist = TRUE,
                                                     x = vec,
                                                     dimnames = list(quanteda::docnames(x), feat_name)))
     attributes(output)[["meta"]] <- x_attrs[["meta"]]
-    if (remove_docvars_dist) {
-        x_docvars$dist <- NULL
+    if (remove_docvars_proximity) {
+        x_docvars$proximity <- NULL
     }
     docvars(output) <- x_docvars
     return(output)
--- tests/testthat/test-docvars.R
+++ tests/testthat/test-docvars.R
@@ -4,12 +4,12 @@ test_that("docvars retention", {
     corpus(test, docvars = data.frame(dummy = TRUE)) -> test_corpus
     meta(test_corpus, "what") <- "test"
     expect_equal(test_corpus %>% tokens() %>% dfm() %>% docvars("dummy"), TRUE)
-    expect_equal(test_corpus %>% tokens() %>% tokens_dist(targets = "world") %>% dfm() %>% docvars("dummy"), TRUE)
+    expect_equal(test_corpus %>% tokens() %>% tokens_proximity(keywords = "world") %>% dfm() %>% docvars("dummy"), TRUE)
     ## remove_docvars_dist
-    docvars_cols <- test_corpus %>% tokens() %>% tokens_dist(targets = "world") %>% dfm(remove_docvars_dist = FALSE) %>% docvars() %>% colnames()
-    expect_true("dist" %in% docvars_cols)
-    docvars_cols <- test_corpus %>% tokens() %>% tokens_dist(targets = "world") %>% dfm(remove_docvars_dist = TRUE) %>% docvars() %>% colnames()
-    expect_false("dist" %in% docvars_cols)
+    docvars_cols <- test_corpus %>% tokens() %>% tokens_proximity(keywords = "world") %>% dfm(remove_docvars_proximity = FALSE) %>% docvars() %>% colnames()
+    expect_true("proximity" %in% docvars_cols)
+    docvars_cols <- test_corpus %>% tokens() %>% tokens_proximity(keywords = "world") %>% dfm(remove_docvars_proximity = TRUE) %>% docvars() %>% colnames()
+    expect_false("proximity" %in% docvars_cols)
 })
 
 test_that("meta retention", {
@@ -18,5 +18,5 @@ test_that("meta retention", {
     corpus(test, docvars = data.frame(dummy = TRUE)) -> test_corpus
     meta(test_corpus, "what") <- "test"
     expect_equal(test_corpus %>% tokens() %>% dfm() %>% meta("what"), "test")
-    expect_equal(test_corpus %>% tokens() %>% tokens_dist(targets = "world") %>% dfm() %>% meta("what"), "test")
+    expect_equal(test_corpus %>% tokens() %>% tokens_proximity(keywords = "world") %>% dfm() %>% meta("what"), "test")
 })
